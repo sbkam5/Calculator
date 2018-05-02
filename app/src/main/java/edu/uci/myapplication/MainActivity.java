5694b78fe03ae889b7792d143acfc325ccf2f961
akyupely <akyupely@uci.edu>
2018-05-02 10:23:25 -0700

    pulling

--- app/src/main/java/edu/uci/myapplication/MainActivity.java
+++ app/src/main/java/edu/uci/myapplication/MainActivity.java
@@ -10,12 +10,27 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*Button button1, button2, button3, button4, button5, button6, button7, button8, button9,
-        buttonBack, buttonClear, buttonDivide, buttonMult, buttonAdd, buttonSubtract;*/
+    Button button, button2, button3, button4, button5, button6, button7, button8, button9,
+        buttonBack, buttonClear, buttonDivide, buttonMult, buttonAdd, buttonSubtract;
+    EditText edit1;
+    float value1, value2;
+    boolean add, sub, mult, div;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        buttonBack = (Button) findViewById(R.id.button2);
+        buttonClear = (Button) findViewById(R.id.buttonClear);
     }
 }
