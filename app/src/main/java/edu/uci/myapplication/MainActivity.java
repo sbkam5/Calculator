7a8750b0898d2c3b767571f85fd59ecebd04d37e
Spencer Brendan Kam <sbkam@uci.edu>
2018-05-02 16:29:59 -0700

    Almost fully operational.  Only the parenthesis buttons need to be added in.

--- app/src/main/java/edu/uci/myapplication/MainActivity.java
+++ app/src/main/java/edu/uci/myapplication/MainActivity.java
@@ -16,10 +16,12 @@ public class MainActivity extends AppCompatActivity {
     //declare variable buttons and text editor
     Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9,
         buttonBack, buttonClear, buttonDivide, buttonMult, buttonAdd, buttonSubtract, buttonEqual,
-        buttonDecimal, buttonPercent;
+        buttonDecimal, buttonPercent, startP, endP;
 
     EditText edt1;
 
+    Boolean backSpace = false;
+    Boolean starting = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,76 +59,209 @@ public class MainActivity extends AppCompatActivity {
                 eq = Double.toString(result);
 
                 edt1.setText(eq);
+
+                starting = true;      //reset starting conditions of code
+                backSpace = false;
             }
         });
 
         button0.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                edt1.setText(edt1.getText() + "0 ");
+                if(starting){
+                    edt1.setText("0 ");  //if the app is atrting out, you can directly input number
+                }
+                else if(!backSpace) {
+                    edt1.setText(edt1.getText() + "0 ");  //if backspace is not true, just add in the number
+                }
+                else{
+                    String temp = edt1.getText().toString();  //if backspace is true, do a backspace before entering in a number
+                    int len = temp.length();
+                    temp = temp.substring(0,len-1);
+                    edt1.setText(temp + "0 ");
+                }
+                backSpace = true;
+                starting = false;
             }
         });
 
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                edt1.setText(edt1.getText() + "1 ");
+            public void onClick(View v){  //look at comments for button zero
+                if(starting){
+                    edt1.setText("1 ");
+                }
+                else if(!backSpace) {
+                    edt1.setText(edt1.getText() + "1 ");
+                }
+                else{
+                    String temp = edt1.getText().toString();
+                    int len = temp.length();
+                    temp = temp.substring(0,len-1);
+                    edt1.setText(temp + "1 ");
+                }
+                backSpace = true;
+                starting = false;
             }
         });
 
         button2.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                edt1.setText(edt1.getText() + "2 ");
+            public void onClick(View v){  //look at comments for button zero
+                if(starting){
+                    edt1.setText("2 ");
+                }
+                else if(!backSpace) {
+                    edt1.setText(edt1.getText() + "2 ");
+                }
+                else{
+                    String temp = edt1.getText().toString();
+                    int len = temp.length();
+                    temp = temp.substring(0,len-1);
+                    edt1.setText(temp + "2 ");
+                }
+                backSpace = true;
+                starting = false;
             }
         });
 
         button3.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                edt1.setText(edt1.getText() + "3 ");
+            public void onClick(View v){  //look at comments for button zero
+                if(starting){
+                    edt1.setText("3 ");
+                }
+                else if(!backSpace) {
+                    edt1.setText(edt1.getText() + "3 ");
+                }
+                else{
+                    String temp = edt1.getText().toString();
+                    int len = temp.length();
+                    temp = temp.substring(0,len-1);
+                    edt1.setText(temp + "3 ");
+                }
+                backSpace = true;
+                starting = false;
             }
         });
 
         button4.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                edt1.setText(edt1.getText() + "4 ");
+            public void onClick(View v){  //look at comments for button zero.
+                if(starting){
+                    edt1.setText("4 ");
+                }
+                else if(!backSpace) {
+                    edt1.setText(edt1.getText() + "4 ");
+                }
+                else{
+                    String temp = edt1.getText().toString();
+                    int len = temp.length();
+                    temp = temp.substring(0,len-1);
+                    edt1.setText(temp + "4 ");
+                }
+                backSpace = true;
+                starting = false;
             }
         });
 
         button5.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                edt1.setText(edt1.getText() + "5 ");
+            public void onClick(View v){  //look at comments for button zero.
+                if(starting){
+                    edt1.setText("5 ");
+                }
+                else if(!backSpace) {
+                    edt1.setText(edt1.getText() + "5 ");
+                }
+                else{
+                    String temp = edt1.getText().toString();
+                    int len = temp.length();
+                    temp = temp.substring(0,len-1);
+                    edt1.setText(temp + "5 ");
+                }
+                backSpace = true;
+                starting = false;
             }
         });
 
         button6.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                edt1.setText(edt1.getText() + "6 ");
+            public void onClick(View v){  //look at comments for button zero.
+                if(starting){
+                    edt1.setText("6 ");
+                }
+                else if(!backSpace) {
+                    edt1.setText(edt1.getText() + "6 ");
+                }
+                else{
+                    String temp = edt1.getText().toString();
+                    int len = temp.length();
+                    temp = temp.substring(0,len-1);
+                    edt1.setText(temp + "6 ");
+                }
+                backSpace = true;
+                starting = false;
             }
         });
 
         button7.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                edt1.setText(edt1.getText() + "7 ");
+            public void onClick(View v){  //look at comments for button zero.
+                if(starting){
+                    edt1.setText("7 ");
+                }
+                else if(!backSpace) {
+                    edt1.setText(edt1.getText() + "7 ");
+                }
+                else{
+                    String temp = edt1.getText().toString();
+                    int len = temp.length();
+                    temp = temp.substring(0,len-1);
+                    edt1.setText(temp + "7 ");
+                }
+                backSpace = true;
+                starting = false;
             }
         });
 
         button8.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                edt1.setText(edt1.getText() + "8 ");
+            public void onClick(View v){  //look at comments for button zero.
+                if(starting){
+                    edt1.setText("8 ");
+                }
+                else if(!backSpace) {
+                    edt1.setText(edt1.getText() + "8 ");
+                }
+                else{
+                    String temp = edt1.getText().toString();
+                    int len = temp.length();
+                    temp = temp.substring(0,len-1);
+                    edt1.setText(temp + "8 ");
+                }
+                backSpace = true;
+                starting = false;
             }
         });
 
         button9.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                edt1.setText(edt1.getText() + "9 ");
+            public void onClick(View v){  //look at comments for button zero
+                if(starting){
+                    edt1.setText("9 ");
+                }
+                else if(!backSpace) {
+                    edt1.setText(edt1.getText() + "9 ");
+                }
+                else{
+                    String temp = edt1.getText().toString();
+                    int len = temp.length();
+                    temp = temp.substring(0,len-1);
+                    edt1.setText(temp + "9 ");
+                }
+                backSpace = true;
+                starting = false;
             }
         });
 
@@ -134,6 +269,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 edt1.setText(edt1.getText() + "+ ");
+                backSpace = false;  //never backspace after an operator. Also program is no longer starting
+                starting = false;
             }
         });
 
@@ -141,6 +278,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 edt1.setText(edt1.getText() + "- ");
+                backSpace = false;  //look at comment for Add button
+                starting = false;
             }
         });
 
@@ -148,6 +287,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 edt1.setText(edt1.getText() + "* ");
+                backSpace = false;  //look at comments for add button
+                starting = false;
             }
         });
 
@@ -155,6 +296,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 edt1.setText(edt1.getText() + "/ ");
+                backSpace = false;  //look at comments for add button
+                starting = false;
             }
         });
 
@@ -163,29 +306,55 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v){
                 String temp = edt1.getText().toString();
                 int strlen = temp.length();
-                temp = temp.substring(0,strlen);
-                edt1.setText(temp +  ".");
+                temp = temp.substring(0,strlen-1);  //decimal should always be input after a number, so a backspace is mandatory.
+                edt1.setText(temp +  ". ");
+                backSpace = true;
+                starting = false;
             }
         });
+
+        //when user clicks on percent sign, a percent will be added
+        buttonPercent.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String temp = edt1.getText().toString();
+                temp = percent(temp);  //call percent function on string to convert the string
+                edt1.setText(temp);
+                backSpace = true;
+                starting = false;
+            }
+        });
+
+        //when user clicks on "back" button, a backspace will be simulated.
+        buttonBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String temp = edt1.getText().toString();
+                int strlen = temp.length();
+                temp = temp.substring(0,strlen-1);
+                edt1.setText(temp);
+                backSpace = false;
+                starting = false;
+            }
+        });
+
+        //when user clicks on "AC", the entire line will be cleared.
+        buttonClear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                edt1.setText("0");
+                starting = true;
+                backSpace = false;
+            }
+        });
+
+
     }
 
     //this method splits a string into an arraylist of strings to calculate and returns the calcualted result
     public static double calc(String s){
         String[] e = s.split(" ");
         ArrayList<String> a = new ArrayList<String>(Arrays.asList(e));
-
-        int perc = a.indexOf("%"); //search for any percent symbols to replace
-        while(perc > -1){
-            //find substring to replace
-            double temp = Double.parseDouble(a.get(perc-1));  //number always goes before percent
-            temp /= 100;
-            String tempString = Double.toString(temp);  //find double equivalent of percent string
-
-            a.remove(perc-1);  // replace percent with double
-            a.remove(perc-1);
-            a.add(perc-1, tempString);
-            perc = a.indexOf("%");
-        }
 
         int length = a.size();
         int firstIndex = -1;
@@ -237,6 +406,28 @@ public class MainActivity extends AppCompatActivity {
                 default: //do nothing on numbers
                     break;
             }
+        }
+
+        return r;
+    }
+
+    //converts a number to its percent equivalent
+    public static String percent(String s){
+
+        String[] e = s.split(" ");
+        ArrayList<String> a = new ArrayList<String>(Arrays.asList(e));
+        int len = a.size(); //find size of arraylist.  The number to replace will be at the end
+
+            double temp = Double.parseDouble(a.get(len-1));  //number always goes before percent
+            temp /= 100;
+            String tempString = Double.toString(temp);  //find double equivalent of percent string
+
+            a.remove(len-1);  // replace last num with its percent equivalent
+            a.add(tempString);
+
+        String r = a.get(0) + " ";
+        for(int i = 1; i < len; i++){
+            r = r.concat(a.get(i) + " ");
         }
 
         return r;
